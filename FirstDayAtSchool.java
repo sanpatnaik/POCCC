84ea8fd4d85455ae9a3ad872d0300226b0422e54
Apadda <apadda@gmail.com>
2017-03-15 15:35:39 +0530

    Git ignore file

--- FirstDayAtSchool.java
+++ FirstDayAtSchool.java
@@ -5,13 +5,13 @@ public class FirstDayAtSchool {
 
 	public String[] prepareMyBag() {
 		String[] schoolbag = {"Books", "Notebookss", "Penss"};
-		System.out.println("My school bag contains: "+Arrays.toString(schoolbag));
-		return schoolbag;
+		System.out.println("My(Aman) school bag contains: "+Arrays.toString(schoolbag));
+		return schoolbags;
 	}
 
 	public String[] addPencils() {
 		String[] schoolbag = {"Books", "Notebookss", "Penss", "Pencilss"};
-		System.out.println("Now my school bag contain: "+Arrays.toString(schoolbag));
-		return schoolbag;
+		System.out.println("Now my(Aman) school bag contain: "+Arrays.toString(schoolbag));
+		return schoolbags;
 	}
 }
