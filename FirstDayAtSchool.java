fdc1e4aa255c9f95e977ccba178e889461c52a8a
sanpatnaik <sandeeppatnaik1991@gmail.com>
2017-03-16 11:21:12 +0530

    sandeep

--- FirstDayAtSchool.java
+++ FirstDayAtSchool.java
@@ -5,13 +5,13 @@ public class FirstDayAtSchool {
 
 	public String[] prepareMyBag() {
 		String[] schoolbag = {"Books", "Notebookss", "Penss"};
-		System.out.println("My(Aman) school bag contains: "+Arrays.toString(schoolbag));
+		System.out.println("My(Amans)(Sandeep) school bag contains: "+Arrays.toString(schoolbag));
 		return schoolbags;
 	}
 
 	public String[] addPencils() {
 		String[] schoolbag = {"Books", "Notebookss", "Penss", "Pencilss"};
-		System.out.println("Now my(Aman) school bag contain: "+Arrays.toString(schoolbag));
+		System.out.println("Now my(Amans)(Sandeep) school bag contain: "+Arrays.toString(schoolbag));
 		return schoolbags;
 	}
 }
