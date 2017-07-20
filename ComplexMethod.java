1558e1afb7821e0424becd0c5b78140f26146768
Pusahu <pushpendrasahu@gmail.com>
2017-07-20 16:05:21 +0530

    Update ComplexMethod.java

--- ComplexMethod.java
+++ ComplexMethod.java
@@ -49,6 +49,11 @@ public class ComplexMethod {
         if (i > 10) {
             throw new NotImplementedException();
         }
+			    int target = -5;
+int num = 3;
+
+target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+target =+ num;
         return null;
     }
 
